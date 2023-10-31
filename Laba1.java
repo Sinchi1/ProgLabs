ee88c204688d9b8f3c8a543693d4d7914762a129
Sinchi <truskovsky2017@yandex.ru>
2023-10-31 17:57:38 +0300

    Update Laba1.java

--- Laba1.java
+++ Laba1.java
@@ -1,19 +1,17 @@
-import java.util.Random;
-
 public class Laba1{
 	public static void main(String[] args){
-		Random rn = new Random();
-		short [] c = new short[12];
+		Double arr [][] = new Double[12][13];
+		double x [] = new double[14];
+		short c [] = new short[12];
 		int j = 0;
 		for (short i = 6; i <= 17 ; i++){
 			c[j] = i;
 			j++; 
 		}
-		double [] x = new double[14];
 		for (int i = 0; i < 14; i++){
-			x[i] = rn.nextDouble(23)-13;
+			x[i] = (Math.random()*23)-13;
+			// Отрезок с 0 до 23, - 13 даёт нам нужное множество
 		}
-		Double arr [][] = new Double[12][13];
 		for (int i = 0; i < arr.length;i++){
 			for (int v = 0; v < arr[i].length;v++){
 				switch(c[i]){
@@ -29,12 +27,20 @@ public class Laba1{
 						arr[i][v] = 0.25 * Math.sin(Math.pow(x[v],3.0));
 						break;
 					default:
-						arr[i][v] = Math.pow((1/3)*Math.pow(Math.E,x[v])*(1-Math.pow(Math.E,Math.log10(Math.pow(Math.sin(x[v]),2)))),2);
-
+						arr[i][v] = Math.pow( 1.0/3 * Math.pow(Math.E,x[v])*(1-Math.pow(Math.E,Math.log10(Math.pow(Math.sin(x[v]),2)))),2);
+						break;
 				}
-
-				
 			}
 		}
-	}
+		// Вывод Ответа 
+		for (int i = 0; i < arr.length;i++){
+			for (int v = 0; v < arr[i].length; v++){
+				if (v == 12){
+					System.out.printf("%.3f" + "\n",arr[i][v]);	
+				}
+				System.out.printf("%.3f" + "  ",arr[i][v]);
+			}
+		}
+
+	}   
 }
